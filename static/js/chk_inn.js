73f7e4fa2290e65a8a76a3eb89e076833d2e187a
ti.eugene <ti.eugene@gmail.com>
2014-08-18 17:59:39 +0000

    js checkINN ok

--- static/js/chk_inn.js
+++ static/js/chk_inn.js
@@ -1,56 +1,78 @@
-function checkINN(inputNumber){
-    //преобразуем в строку
-    inputNumber = "" + inputNumber;
-    //преобразуем в массив
-    inputNumber = inputNumber.split('');
-    //для ИНН в 10 знаков
-    if((inputNumber.length == 10) && (inputNumber[9] == ((2 * inputNumber[  0] + 4 * inputNumber[1] + 10 * inputNumber[2] + 3 * inputNumber[3] + 5 * inputNumber[4] + 9 * inputNumber[5] + 4 * inputNumber[6] + 6 * inputNumber[7] + 8 * inputNumber[8]) % 11) % 10)){
-        return true;
-    //для ИНН в 12 знаков
-    }else if((inputNumber.length == 12) && ((inputNumber[10] == ((7 * inputNumber[ 0] + 2 * inputNumber[1] + 4 * inputNumber[2] + 10 * inputNumber[3] + 3 * inputNumber[4] + 5 * inputNumber[5] + 9 * inputNumber[6] + 4 * inputNumber[7] + 6 * inputNumber[8] + 8 * inputNumber[9]) % 11) % 10) && (inputNumber[11] == ((3 * inputNumber[ 0] + 7 * inputNumber[1] + 2 * inputNumber[2] + 4 * inputNumber[3] + 10 * inputNumber[4] + 3 * inputNumber[5] + 5 * inputNumber[6] + 9 * inputNumber[7] + 4 * inputNumber[8] + 6 * inputNumber[9] + 8 * inputNumber[10]) % 11) % 10))){
-        return true;
-    }else{
-        return false;
-    }
-}
-//
-$.validator.addMethod("checkINN", function(value, elem) {
-    var inputNumber = $(elem).val();  // или value
-    //преобразуем в строку
-    inputNumber = "" + inputNumber;
-    //преобразуем в массив
-    inputNumber = inputNumber.split('');
-    //для ИНН в 10 знаков
-    if((inputNumber.length == 10) &&
-        (inputNumber[9] ==
-            ((2 * inputNumber[  0] + 4 * inputNumber[1] + 10 *
-             inputNumber[2] + 3 * inputNumber[3] + 5 *
-             inputNumber[4] + 9 * inputNumber[5] + 4 *
-             inputNumber[6] + 6 * inputNumber[7] + 8 *
-             inputNumber[8]) % 11) % 10))
-    {
-        return true;
-    //для ИНН в 12 знаков
-    }
-    else if((inputNumber.length == 12) &&
-        ((inputNumber[10] == ((7 * inputNumber[ 0] + 2 *
-        inputNumber[1] + 4 * inputNumber[2] + 10 *
-        inputNumber[3] + 3 * inputNumber[4] + 5 *
-        inputNumber[5] + 9 * inputNumber[6] + 4 *
-        inputNumber[7] + 6 * inputNumber[8] + 8 *
-        inputNumber[9]) % 11) % 10) &&
-        (inputNumber[11] == ((3 * inputNumber[ 0] + 7 *
-         inputNumber[1] + 2 * inputNumber[2] + 4 *
-         inputNumber[3] + 10 * inputNumber[4] + 3 *
-         inputNumber[5] + 5 * inputNumber[6] + 9 *
-         inputNumber[7] + 4 * inputNumber[8] + 6 *
-         inputNumber[9] + 8 * inputNumber[10]) % 11) % 10)))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+function INN10crc (v) {
+	return (v[9] == (
+		(
+			 2 * v[0] +
+			 4 * v[1] +
+			10 * v[2] +
+			 3 * v[3] +
+			 5 * v[4] +
+			 9 * v[5] +
+			 4 * v[6] +
+			 6 * v[7] +
+			 8 * v[8]
+		) % 11) % 10
+	);
+};
 
-    },"Введите корректный ИНН");
+function INN12crc (v) {
+	return (
+		(v[10] == (
+			(
+				 7 * v[0] +
+				 2 * v[1] +
+				 4 * v[2] +
+				10 * v[3] +
+				 3 * v[4] +
+				 5 * v[5] +
+				 9 * v[6] +
+				 4 * v[7] +
+				 6 * v[8] +
+				 8 * v[9]
+			) % 11) % 10
+		) &&
+		(v[11] == (
+			(
+				 3 * v[0] +
+				 7 * v[1] +
+				 2 * v[2] +
+				 4 * v[3] +
+				10 * v[4] +
+				 3 * v[5] +
+				 5 * v[6] +
+				 9 * v[7] +
+				 4 * v[8] +
+				 6 * v[9] +
+				 8 * v[10]) % 11) % 10
+		)
+	);
+};
+
+function checkINN (v) {
+	// Check input string as INN:
+	// 1. empty
+	// 2. digits only
+	// 3. 10 or 12 symbols
+	// 4. checksumms
+	// Returns:
+	// ok: null
+	// err: string
+	var pattern = /^\d+$/;
+	v = "" + v;	// convert to str
+	// 1. length
+	if (v.length == 0) {
+		return "ИНН не может быть пустым";
+	}
+	// 2. digits only
+	if (!pattern.test(v)) {
+		return "Только цифры";
+	};
+	// 3. length
+	if ((v.length != 10) && (v.length != 12)) {
+		return "10 или 12 символов";
+	};
+	v = v.split('');
+	if (((v.length == 10) && (INN10crc(v) == false)) || ((v.length == 12) && (INN12crc(v) == false))) {
+		return "Такого ИНН не может быть";
+	};
+	return null;
+};
