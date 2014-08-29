9c654623bc446f809974444038a8d014fbb58f52
ti.eugene <ti.eugene@gmail.com>
2014-08-29 10:27:13 +0000

    models char fields max_len tuned; scan.admin update

--- scan/admin.py
+++ scan/admin.py
@@ -3,10 +3,14 @@ from django.contrib import admin
 from models import *
 
 # 1. inlines
+class   EventInLine(admin.TabularInline):
+        model           = Event
+        extra           = 1
 
 # 2. odmins
 class	ScanAdmin(admin.ModelAdmin):
 	ordering	= ('fileseq',)
 	list_display	= ('fileseq', 'place', 'subject', 'depart', 'shipper', 'supplier', 'no', 'date', 'sum')
+	inlines		= (EventInLine,)
 
 admin.site.register(Scan,	ScanAdmin)
