d170375068df86833bc87cdc1fa8a9c56f6d2825
ti.eugene <ti.eugene@gmail.com>
2014-08-31 07:35:55 +0000

    flush

--- refresh_db.sh
+++ refresh_db.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+# Refresh DB (sqlite spec)
 echo "1. dump data (30'')"
-time ./manage.py dumpdata --format=json --indent=1 > data.json
+time ./manage.py dumpdata --format=json --indent=1 | gzip -c > data.json.gz
 echo "2. drop tables"
 time for i in `echo ".tables" | ./manage.py dbshell`; do echo "DROP TABLE IF EXISTS $i;" | ./manage.py dbshell; done
 echo "3. recreate db"
