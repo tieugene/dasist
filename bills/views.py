3b8a9372fec5d7c6a1ce5106086396a51cbb2351
ti.eugene <ti.eugene@gmail.com>
2014-11-10 08:36:28 +0000

    0.1.5 started; some changes (e.g. blls.views ACLs)

--- bills/views.py
+++ bills/views.py
@@ -37,6 +37,12 @@ logger = logging.getLogger(__name__)
 PAGE_SIZE = 25
 FSNAME = 'fstate'	# 0..3
 
+STATE_DRAFT	= 1
+STATE_ONWAY	= 2
+STATE_REJECTED	= 3
+STATE_ONPAY	= 4
+STATE_DONE	= 5
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -275,7 +281,6 @@ def	bill_edit(request, id):
 	}))
 
 @login_required
-#transaction.commit_on_success
 @transaction.atomic
 def	bill_reedit(request, id):
 	'''
@@ -321,96 +326,147 @@ def	bill_reedit(request, id):
 	}))
 
 @login_required
-#transaction.commit_on_success
 @transaction.atomic
 def	bill_view(request, id, upload_form=None):
 	'''
 	View | Accept/Reject bill
 	ACL: (assignee & Draft & Route ok) | (approver & OnWay)
-	= POST =
-	- Исполнитель & Draft
-	- Руководитель & Подписант [& Onway]
-	- Директор & Подписант.Роль [& Onway]
-	- Гендир & Подписант [& Onway]
-	- Бухгалтер & Подписант.Роль [& Onway]
-	= GET =
-	- root
-	- Исполнитель: только свои (где он - Исполнитель)
-	- Руководитель: только где он - текущий Подписант или в Истории
-	- *: все
+	- POST (Draft)
+	-- Исполнитель & Draft
+	-- Руководитель & Подписант онже [& Onway]
+	-- Директор & Подписант.Роль егоже [& Onway]
+	-- Бухгалтер & Подписант.Роль [& (Onway/Onpay)]
+	-- Гендир & Подписант онже [& Onway]
+	- POST (upload):
+	-- user == Исполнитель & Draft
+	- POST
+	-- user == Исполнитель & Draft
+	-- user.role == Директор | Бухгалтер == Подписант.Роль
+	-- user == Подписант
+	- GET
+	-- root
+	-- Исполнитель: user == Исполнитель
+	-- Руководитель: user == Подписант или в Истории
+	-- *: все
 	'''
-	#bill = models.Bill.objects.get(pk=int(id))
-	bill = get_object_or_404(models.Bill, pk=int(id))
-	user = request.user
-	logger.info('bill_view: user: %s, bill: %s' % (user.username, id))
-	approver = models.Approver.objects.get(user=user)
-	bill_state_id = bill.get_state_id()
-	form = None
-	upload_form = None
-	err = ''
-	if (request.method == 'POST'):
-	    if request.POST['action'] == 'upload':
+	def	__can_upload(bill, approver):
+		'''
+		ACL to uppload img.
+		user == bill.approver && bill.state == Draft
+		'''
+		return (approver == bill.assign) and (bill.get_state_id() == 1)
+
+	def	__upload(request, bill):
+		'''
+		Upload file to Bill
+		@param request
+		@parma bill:models.Bill
+		@return upload_form
+		'''
 		upload_form = forms.BillAddFileForm(request.POST, request.FILES)
 		if upload_form.is_valid():
 			file = request.FILES.get('file', None)
 			if (file):
 				fileseq = bill.fileseq
 				update_fileseq(file, fileseq, upload_form.cleaned_data['rawpdf'])	# unicode error
-	    else:
-		#if (request.POST['resume'] in set(['accept', 'reject'])) and (\
-		#   ((bill_state_id == 1) and (approver == bill.assign)) or\
-		#   (((bill_state_id == 2) or (bill_state_id == 3)) and ( \
-		#	((bill.rpoint.approve != None) and (approver == bill.rpoint.approve)) or\
-		#	((bill.rpoint.approve == None) and (approver.role == bill.rpoint.role))\
-		#    ) \
-		#    )
-		#   ):
-		if (True):	# dummy ACL
+		return upload_form
+	def	__can_resume(request, bill, approver):
+		'''
+		ACL to resume.
+		- Draft and user == bill assign
+		- OnWay and ((user == bill.rpoint.user) or (user.role == bill.rpoint.role))
+		- OnPay and user.role == Accounter
+		'''
+		bill_state_id = bill.get_state_id()
+		return (request.POST['resume'] in set(['accept', 'reject'])) and (\
+		  ((bill_state_id == STATE_DRAFT) and (approver == bill.assign)) or \
+		  ((bill_state_id == STATE_ONWAY) and ( \
+			((bill.rpoint.approve != None) and (approver == bill.rpoint.approve)) or\
+			((bill.rpoint.approve == None) and (approver.role == bill.rpoint.role))\
+		   ) \
+		  ) or \
+		  ((bill_state_id == STATE_ONPAY) and (approver.role == bill.rpoint.role)) \
+		)
+
+	def	__resume(request, bill):
+		'''
+		@return resume_form, err[, redirect[ url]
+		'''
+		approver = models.Approver.objects.get(user=request.user)
+		bill_state_id = bill.get_state_id()
+		ok = False
+		err = None
+		resume_form = forms.ResumeForm(request.POST)
+		if resume_form.is_valid():
 			resume = (request.POST['resume'] == 'accept')
-			form = forms.ResumeForm(request.POST)
-			if form.is_valid():
-				# 0. check prerequisites
-				if (not resume) and (not form.cleaned_data['note']):				# resume not empty on reject
-					err = 'Отказ необходимо комментировать'
-				else:
-					# 1. new comment
-					models.Event.objects.create(
-						bill=bill,
-						approve=approver,
-						resume=resume,
-						comment=form.cleaned_data['note']
-					)
-					# 2. update bill
-					if resume:	# Ok
-						route_list = bill.route_set.all().order_by('order')
-						if (bill_state_id == 1):	# 1. 1st (draft)
-							bill.rpoint = route_list[0]
-							bill.set_state_id(2)
-						elif (bill_state_id == 2):	# 2. onway
-							rpoint = bill.rpoint
-							if (rpoint.order == len(route_list)):	# 2. last (accounter)
-								bill.set_state_id(4)
-							else:					# 3. intermediate
-								bill.rpoint = bill.route_set.get(order=rpoint.order+1)
-						elif (bill_state_id == 4):	# OnPay
-							bill.rpoint = None
-							bill.payedsum += bill.topaysum
-							bill.topaysum = decimal.Decimal('0.00')
-							bill.set_state_id(5)
-							bill.locked = (bill.payedsum < bill.billsum)
-					else:		# Reject
-						bill.set_state_id(3)
+			# 0. check prerequisites
+			if (not resume) and (not resume_form.cleaned_data['note']):			# resume not empty on reject
+				err = 'Отказ необходимо комментировать'
+			else:
+				# 1. new comment
+				models.Event.objects.create(
+					bill=bill,
+					approve=approver,
+					resume=resume,
+					comment=resume_form.cleaned_data['note']
+				)
+				# 2. update bill
+				if resume:	# Ok
+					route_list = bill.route_set.all().order_by('order')
+					if (bill_state_id == 1):	# 1. 1st (draft)
+						bill.rpoint = route_list[0]
+						bill.set_state_id(2)
+					elif (bill_state_id == 2):	# 2. onway
+						rpoint = bill.rpoint
+						if (rpoint.order == len(route_list)):	# 2. last (accounter)
+							bill.set_state_id(4)
+						else:					# 3. intermediate
+							bill.rpoint = bill.route_set.get(order=rpoint.order+1)
+					elif (bill_state_id == 4):	# OnPay
 						bill.rpoint = None
-					bill.save()
-					if (bill.get_state_id() == 5) and (bill.locked == False):	# That's all
-						bill.route_set.all().delete()
-					mailto(request, bill)
+						bill.payedsum += bill.topaysum
+						bill.topaysum = decimal.Decimal('0.00')
+						bill.set_state_id(5)
+						bill.locked = (bill.payedsum < bill.billsum)
+				else:		# Reject
+					bill.set_state_id(3)
+					bill.rpoint = None
+				bill.save()
+				if (bill.get_state_id() == 5) and (bill.locked == False):	# That's all
+					bill.route_set.all().delete()
+				mailto(request, bill)
+				ok = True
+		return (ok, resume_form, err)
+	def	__can_accept():
+		pass
+	def	__accept():
+		pass
+	def	__can_reject():
+		pass
+	def	__reject():
+		pass
+	bill = get_object_or_404(models.Bill, pk=int(id))
+	user = request.user
+	logger.info('bill_view: user: %s, bill: %s' % (user.username, id))
+	approver = models.Approver.objects.get(user=user)
+	bill_state_id = bill.get_state_id()
+	upload_form = None
+	resume_form = None
+	err = ''
+	if (request.method == 'POST'):
+		if request.POST['action'] == 'upload':
+			if (__can_upload(bill, approver)):
+				upload_form = __upload(request, bill)
+		else:	# resume
+			if (__can_resume(request, bill, approver)):
+				ok, resume_form, err = __resume(request, bill)
+				if (ok):
 					return redirect('bill_list')
-	else:
+	else:	# GET
 		if (user.is_superuser or ((bill.assign == approver) and (bill_state_id == 1))):
 			upload_form = forms.BillAddFileForm()
-	if (form == None):
-		form = forms.ResumeForm()
+	if (resume_form == None):
+		resume_form = forms.ResumeForm()
 	buttons = {
 		'edit':		# assignee & Draft*
 			(user.is_superuser or ((bill.assign == approver) and (bill_state_id == 1))),
@@ -423,17 +479,18 @@ def	bill_view(request, id, upload_form=None):
 	}
 	# Accepting (Вперед, Согласовано, В оплате, Исполнено)
 	buttons['accept'] = 0
-	if (bill_state_id == 1):			# Draft
+	if (bill_state_id == STATE_DRAFT):			# Draft
 		if (bill.assign == approver):
 			buttons['accept'] = 1		# Вперед
-	elif (bill_state_id == 2):			# OnWay
+	elif (bill_state_id == STATE_ONWAY):		# OnWay
 		if (approver.role.pk != 6):		# not Accounter
 			if ((bill.rpoint.approve == None) and (bill.rpoint.role == approver.role)) or \
 			   ((bill.rpoint.approve != None) and (bill.rpoint.approve == approver)):
 				buttons['accept'] = 2	# Согласовано
 		else:					# Accounter
-			buttons['accept'] = 3		# В оплате
-	elif (bill_state_id == 4):			# OnPay
+			if (bill.rpoint.role == approver.role):
+				buttons['accept'] = 3		# В оплате
+	elif (bill_state_id == STATE_ONPAY):		# OnPay
 		if (approver.role.pk == 6):		# Accounter
 			buttons['accept'] = 4		# Оплачен
 	# Rejecting
@@ -447,7 +504,7 @@ def	bill_view(request, id, upload_form=None):
 			buttons['reject'] = 1
 	return render_to_response('bills/detail.html', context_instance=RequestContext(request, {
 		'object': bill,
-		'form': form if (buttons['accept'] or buttons['reject']) else None,
+		'form': resume_form if (buttons['accept'] or buttons['reject']) else None,
 		'upload_form': upload_form,
 		'err': err,
 		'button': buttons,
