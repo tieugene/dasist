6d6748018546a1118cc139e5d42629c1580f27eb
TI_Eugene <ti.eugene@gmail.com>
2017-01-24 02:57:51 +0300

    Report.Summary: subjs AJAX

--- reports/forms.py
+++ reports/forms.py
@@ -28,4 +28,18 @@ class FilterLedgerListForm(forms.Form):
 class FilterSummaryListForm(forms.Form):
     place = forms.ChoiceField(choices=EMPTY_VALUE + list(Scan.objects.order_by('place').distinct().values_list('place', 'place')), label=u'Объект', required=False)
     subject = forms.ChoiceField(choices=EMPTY_VALUE + list(Scan.objects.order_by('subject').distinct().values_list('subject', 'subject')), label=u'Подобъект', required=False)
-    year = forms.ChoiceField(choices=EMPTY_VALUE + [('2014', '2014'), ('2015', '2015'), ('2016', '2016'), ('2017', '2017')], label=u'Год', required=False)
+    year = forms.ChoiceField(choices=EMPTY_VALUE + [('2013', '2013'), ('2014', '2014'), ('2015', '2015'), ('2016', '2016'), ('2017', '2017')], label=u'Год', required=False)
+
+    def __init__(self, *args, **kwargs):
+        forms.Form.__init__(self, *args, **kwargs)
+        places = EMPTY_VALUE + list(Scan.objects.order_by('place').distinct().values_list('place', 'place'))
+        if len(places) == 1:
+            self.fields['place'].initial = places[0][0]
+        # place = self.fields['place'].initial or self.initial.get('place') or self._raw_value('place')
+        place = self.fields['place'].initial or self.initial.get('place')
+        if place:
+            # parent is known. Now I can display the matching children.
+            subjects = EMPTY_VALUE + list(Scan.objects.filter(place=place).order_by('subject').distinct().exclude(subject=None).values_list('subject', 'subject'))
+            self.fields['subject'].choices = subjects
+            if len(subjects) == 1:
+                self.fields['subject'].initial = subjects[0][0]
