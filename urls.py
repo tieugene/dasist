fb4e422f5f0865297e4a74ad14124c75017870e0
TI_Eugene <ti.eugene@gmail.com>
2017-04-15 18:30:59 +0300

    Reports added

--- urls.py
+++ urls.py
@@ -18,10 +18,10 @@ urlpatterns = [
     url(r'^logout$',            logout, name='logout'),
     url(r'^$',                  TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^about$',             TemplateView.as_view(template_name='about.html'), name='about'),
+    url(r'^report/',            include('reports.urls')),
     url(r'^core/',              include('core.urls')),
     url(r'^bills/',             include('bills.urls')),
     url(r'^scan/',              include('scan.urls')),
-    url(r'^report/',            include('reports.urls')),
     url(r'^contract/',          include('contract.urls')),
 ]
 urlpatterns += staticfiles_urlpatterns()
